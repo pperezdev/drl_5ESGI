2d6fb13d46939f22162874a79a5581bdf9982bd0
pperezdev <pierrick.perez@free.fr>
2023-01-25 15:19:21 +0100

    build pygames

--- back/services/draw_map.py
+++ back/services/draw_map.py
@@ -1,5 +1,15 @@
+from .file_manager import FileManager
+
+def get_structure(path) -> list:
+    fm = FileManager()
+    data = fm.open_file(path)
+    structure = []
+    for line in data.split('\n'):
+        structure.append(line)
+    return structure
+
 def draw_map(path):
-    pass
+    structure = get_structure(path)
 
 def color_to_number():
     pass
--- back/services/file_manager.py
+++ back/services/file_manager.py
@@ -1,3 +1,6 @@
 class FileManager:
-    def open_file(self):
-        pass
+    def open_file(self, path:str) -> str:
+        data = ""
+        with open(path, 'r') as f:
+            data += f.read()
+        return data
