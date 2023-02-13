5ab03321eac0e0f0ad4482176cb65a14a399b5b3
Quentin Pierson <quentinprs62@gmail.com>
2023-02-13 15:56:18 +0100

    add file for algo random

--- back/__init__.py
+++ back/__init__.py
@@ -3,4 +3,4 @@ from .facades import *
 from .tests import test_execute
 from .services import *
 from .games import Game
-from .ai import QLearning, ModelQLearning, DeepQLearning, DoubleDeepQLearning
+from .ai import QLearning, ModelQLearning, DeepQLearning, DoubleDeepQLearning, AlgoRandom
